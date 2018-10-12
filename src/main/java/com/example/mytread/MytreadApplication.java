b077967644a9ab82fceba3f25491a64c9357d029
lixuesong <632410877@qq.com>
2018-10-12 15:25:30 +0800

    [ADD]创建项目

---
+++ src/main/java/com/example/mytread/MytreadApplication.java
@@ -0,0 +1,12 @@
+package com.example.mytread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytreadApplication.class, args);
+	}
+}
